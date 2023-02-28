6ac54bac1ae2907e3af94ca201fb46ca88ab5e3d
tonyxuan2021 <tonyyexuan@gmail.com>
2023-02-27 21:19:50 -0800

    fetched book by id using lodash from GraphiQL

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+
+let books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The FInal EMpire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earch', genre: 'Sci-Fi', id: '3' },
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -9,4 +18,23 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      // expect user to pass in a id when query for a single book
+      args: { id: { type: GraphQLString } },
+      // when receive the query fires the resolve function, this is the code to get data from db / other source
+      resolve(parent, args) {
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
