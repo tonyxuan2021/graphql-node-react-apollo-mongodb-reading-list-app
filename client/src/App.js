4fb9b93e017b2a8ebda95b34e0fb791b0be434da
tonyxuan2021 <tonyyexuan@gmail.com>
2023-02-28 15:59:49 -0800

    added apollo in React to interact with GraphQL

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import BookList from './components/BookList';
+import { ApolloClient, ApolloProvider, gql } from '@apollo/client';
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
 function App() {
   return (
-    <div className="App">
-      <h1>Ninja's Reading List</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Ninja's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
